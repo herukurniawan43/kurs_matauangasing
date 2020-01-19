7a469516285f9b78aedd243c331a29d8f762ac8b
herukurniawan43 <herukurniawan43@gmail.com>
2020-01-19 21:09:21 +0700

    Initial commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Konversi Mata Uang Asing
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Pilih Mata Uang</q-item-label>
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+           </q-item-section>
+          <q-item-section>
+            <q-item-label>Dollar</q-item-label>
+            </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            </q-item-section>
+          <q-item-section>
+            <q-item-label>Riyal</q-item-label>
+            </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            </q-item-section>
+          <q-item-section>
+            <q-item-label>Yen</q-item-label>
+            </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            </q-item-section>
+          <q-item-section>
+            <q-item-label>Ringgit</q-item-label>
+            </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            </q-item-section>
+          <q-item-section>
+            <q-item-label>Yuan</q-item-label>
+            </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" target="_blank">
+          <q-item-section avatar>
+            </q-item-section>
+          <q-item-section>
+            <q-item-label>Euro</q-item-label>
+            </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+  <h5><center>
+  SELAMAT DATANG DI PROGRAM KONVERSI MATA UANG ASING
+  </center></h5>
+
+    <img alt="Love Money Logo" src="~assets/love_money.jpg">
+  </q-page>
+</template>>
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
